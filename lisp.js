23673af874f0f17a0b2b1cadd4d095a66974a441
Heena <heenaj198@gmail.com>
2019-06-26 20:05:10 +0530

    basic arithmetic evaluations

---
+++ lisp.js
@@ -0,0 +1,85 @@
+function numParser (inp) {
+  let result
+  return (result = inp.match(/^-?\d+\.?\d*/)) && [result[0], spaceparse(inp.slice(result[0].length))]
+}
+function spaceParser (inp) {
+  let regex = /^\s+/
+  inp = inp.replace(regex, '')
+  return inp
+}
+function evaluater (inp) {
+  inp = spaceParser(inp)
+  let result = numParser(inp)
+  if (result !== null) return result
+  inp = spaceParser(inp)
+  if (inp.startsWith('(+')) {
+    inp = inp.slice(2)
+    inp = inp.substring(0, inp.indexOf(')'))
+    return add(inp)
+  }
+  if (inp.startsWith('(-')) {
+    inp = inp.slice(2)
+    inp = inp.substring(0, inp.indexOf(')'))
+    return sub(inp)
+  }
+  if (inp.startsWith('(*')) {
+    inp = inp.slice(2)
+    inp = inp.substring(0, inp.indexOf(')'))
+    return mul(inp)
+  }
+  if (inp.startsWith('(/')) {
+    inp = inp.slice(2)
+    inp = inp.substring(0, inp.indexOf(')'))
+    return div(inp)
+  }
+  if (inp.startsWith('(%')) {
+    inp = inp.slice(2)
+    inp = inp.substring(0, inp.indexOf(')'))
+    return mod(inp)
+  }
+}
+function add (inputs) {
+  inputs = spaceParser(inputs)
+  let valArr = inputs.split(' ')
+  let sum = 0
+  for (let i = 0; i < valArr.length; i++) {
+    sum += Number(valArr[i])
+  }
+  return sum
+}
+function sub (inputs) {
+  inputs = spaceParser(inputs)
+  let valArr = inputs.split(' ')
+  let sub = valArr[0]
+  for (let i = 1; i < valArr.length; i++) {
+    sub -= Number(valArr[i])
+  }
+  return sub
+}
+function mul (inputs) {
+  inputs = spaceParser(inputs)
+  let valArr = inputs.split(' ')
+  let mul = 1
+  for (let i = 0; i < valArr.length; i++) {
+    mul *= Number(valArr[i])
+  }
+  return mul
+}
+function div (inputs) {
+  inputs = spaceParser(inputs)
+  let valArr = inputs.split(' ')
+  let div = valArr[0]
+  for (let i = 1; i < valArr.length; i++) {
+    div /= Number(valArr[i])
+  }
+  return div
+}
+function mod (inputs) {
+  inputs = spaceParser(inputs)
+  let valArr = inputs.split(' ')
+  let mod = valArr[0]
+  for (let i = 1; i < valArr.length; i++) {
+    mod %= Number(valArr[i])
+  }
+  return mod
+}

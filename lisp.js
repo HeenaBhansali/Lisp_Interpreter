2f541f92a0d438c6917de3eadfd9877a53f7b08c
Heena <heenaj198@gmail.com>
2019-06-26 23:00:19 +0530

    added environment

--- lisp.js
+++ lisp.js
@@ -1,87 +1,80 @@
-function numParser (str) {
+function numberParser (str) {
   let regEx = /^-?(0|[\d1-9]\d*)(\.\d+)?(?:[Ee][+-]?\d+)?/
   let value = str.match(regEx)
   if (value !== null) return [value[0] * 1, str.slice(value[0].length)]
-  else return null
+  // else return null
+}
+function stringParser (inp) {
+  let result
+  return (result = inp.match(/^[a-zA-z]\w*/)) && [result[0], spaceParser(inp.slice(result[0].length))]
 }
 function spaceParser (inp) {
   let regex = /^\s+/
   inp = inp.replace(regex, '')
   return inp
 }
-function evaluater (inp) {
-  inp = spaceParser(inp)
-  let result = numParser(inp)
-  if (result !== null) return result
-  inp = spaceParser(inp)
-  if (inp.startsWith('(+')) {
-    inp = inp.slice(2)
-    inp = inp.substring(0, inp.indexOf(')'))
-    return add(inp)
-  }
-  if (inp.startsWith('(-')) {
-    inp = inp.slice(2)
-    inp = inp.substring(0, inp.indexOf(')'))
-    return sub(inp)
-  }
-  if (inp.startsWith('(*')) {
-    inp = inp.slice(2)
-    inp = inp.substring(0, inp.indexOf(')'))
-    return mul(inp)
-  }
-  if (inp.startsWith('(/')) {
-    inp = inp.slice(2)
-    inp = inp.substring(0, inp.indexOf(')'))
-    return div(inp)
-  }
-  if (inp.startsWith('(%')) {
-    inp = inp.slice(2)
-    inp = inp.substring(0, inp.indexOf(')'))
-    return mod(inp)
-  }
+let env = {
+  '+': (...list) => list.reduce((x, y) => x + y),
+  '-': (...list) => list.reduce((x, y) => x - y),
+  '*': (...list) => list.reduce((x, y) => x * y),
+  '/': (...list) => list.reduce((x, y) => x / y),
+  '<': (x, y) => x < y,
+  '>': (x, y) => x > y,
+  '=': (x, y) => x === y,
+  '>=': (x, y) => x >= y,
+  '<=': (x, y) => x <= y,
+  'pi': Math.PI
 }
-function add (inputs) {
-  inputs = spaceParser(inputs)
-  let valArr = inputs.split(' ')
-  let sum = 0
-  for (let i = 0; i < valArr.length; i++) {
-    sum += Number(valArr[i])
+function opParser (inp) {
+  let str = inp.slice(0); let op; let args = []; let val
+  if (env[(op = str[0])] === undefined) return null
+  str = spaceParser(str.slice(1))
+  while (!str.startsWith(')')) {
+    if ((val = numberParser(str))) {
+      args.push(+val[0])
+      str = val[1]
+    } else if ((val = stringParser(str))) {
+      if (env[val[0]] === undefined) return null
+      args.push(env[val[0]])
+      str = val[1]
+    } else if (str[0] === '(') {
+      if (!(val = evaluate(str.slice(0)))) return null
+      args.push(val[0])
+      str = val[1]
+    }
+    if (!str.length) return null
   }
-  return sum
+  return [env[op](...args), str]
 }
-function sub (inputs) {
-  inputs = spaceParser(inputs)
-  let valArr = inputs.split(' ')
-  let sub = valArr[0]
-  for (let i = 1; i < valArr.length; i++) {
-    sub -= Number(valArr[i])
+function expParser (inp) {
+  let str = inp.slice(0); let result
+  while (!str.startsWith(')')) {
+    if (str.match(/^(\+|-|\/|\*|<|>|=|<=|>=)/)) {
+      if (!(result = opParser(spaceParser(str)))) return null
+      str = result[1]
+    } else break
   }
-  return sub
+  return result
 }
-function mul (inputs) {
-  inputs = spaceParser(inputs)
-  let valArr = inputs.split(' ')
-  let mul = 1
-  for (let i = 0; i < valArr.length; i++) {
-    mul *= Number(valArr[i])
+function evaluate (inp) {
+  let str = inp.slice(0); let result; let val
+  if (spaceParser(str.slice(1)) === ')') return ['()', '']
+  while (str.length && !str.startsWith(')')) {
+    if (str.startsWith('(')) {
+      if (!(result = expParser(spaceParser(str.slice(1))))) return null
+      str = result[1]
+    }
+    if ((val = numberParser(str))) {
+      result = [+val[0], val[1]]; break
+    }
+    if ((val = stringParser(str))) {
+      result = (env[val[0]] === undefined ? null : [env[val[0]], val[1]]); break
+    }
+    if (!str[0] === ')') str = spaceParser(str.slice(1))
   }
-  return mul
+  return [result[0], spaceParser(str.slice(1))]
 }
-function div (inputs) {
-  inputs = spaceParser(inputs)
-  let valArr = inputs.split(' ')
-  let div = valArr[0]
-  for (let i = 1; i < valArr.length; i++) {
-    div /= Number(valArr[i])
-  }
-  return div
-}
-function mod (inputs) {
-  inputs = spaceParser(inputs)
-  let valArr = inputs.split(' ')
-  let mod = valArr[0]
-  for (let i = 1; i < valArr.length; i++) {
-    mod %= Number(valArr[i])
-  }
-  return mod
+function eva (input) {
+  let result = evaluate(input)
+  console.log(result ? result[0] : 'Invalid')
 }

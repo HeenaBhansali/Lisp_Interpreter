f9c4291dd047f450afca2a35d2156c036e48ce10
Heena <heenaj198@gmail.com>
2019-06-28 11:41:43 +0530

    added begin and define

--- lisp.js
+++ lisp.js
@@ -1,8 +1,6 @@
-function numberParser (str) {
-  let regEx = /^-?(0|[\d1-9]\d*)(\.\d+)?(?:[Ee][+-]?\d+)?/
-  let value = str.match(regEx)
-  if (value !== null) return [value[0] * 1, str.slice(value[0].length)]
-  // else return null
+function numberParser (inp) {
+  let result
+  return (result = inp.match(/^-?(0|[\d1-9]\d*)(\.\d+)?(?:[Ee][+-]?\d+)?/)) && [result[0], spaceParser(inp.slice(result[0].length))]
 }
 function stringParser (inp) {
   let result
@@ -13,7 +11,7 @@ function spaceParser (inp) {
   inp = inp.replace(regex, '')
   return inp
 }
-let env = {
+var env = {
   '+': (...list) => list.reduce((x, y) => x + y),
   '-': (...list) => list.reduce((x, y) => x - y),
   '*': (...list) => list.reduce((x, y) => x * y),
@@ -25,20 +23,28 @@ let env = {
   '<=': (x, y) => x <= y,
   'pi': Math.PI
 }
-function opParser (inp) {
+function define (inp) {
+  let symbol; let str = inp.slice(0); let val
+  if (!(symbol = stringParser(inp))) return null
+  str = symbol[1]
+  if (!(val = evaluater(str))) return null
+  env[symbol[0]] = val[0]
+  return val[1]
+}
+function operator (inp) {
   let str = inp.slice(0); let op; let args = []; let val
-  if (env[(op = str[0])] === undefined) return null
-  str = spaceParser(str.slice(1))
+  if (env[(op = str.slice(0, str.indexOf(' ')))] === undefined) return null
+  str = spaceParser(str.slice(op.length))
   while (!str.startsWith(')')) {
     if ((val = numberParser(str))) {
       args.push(+val[0])
       str = val[1]
     } else if ((val = stringParser(str))) {
       if (env[val[0]] === undefined) return null
-      args.push(env[val[0]])
+      args.push(Number(env[val[0]]))
       str = val[1]
     } else if (str[0] === '(') {
-      if (!(val = evaluate(str.slice(0)))) return null
+      if (!(val = evaluater(str))) return null
       args.push(val[0])
       str = val[1]
     }
@@ -46,35 +52,42 @@ function opParser (inp) {
   }
   return [env[op](...args), str]
 }
-function expParser (inp) {
+function expression (inp) {
   let str = inp.slice(0); let result
   while (!str.startsWith(')')) {
-    if (str.match(/^(\+|-|\/|\*|<|>|=|<=|>=)/)) {
-      if (!(result = opParser(spaceParser(str)))) return null
+    if (str.startsWith('begin')) {
+      if (!(result = evaluater(spaceParser(inp.slice(5))))) return null
+      str = result[1]
+    } else if (str.startsWith('define')) {
+      if (!(str = define(spaceParser(inp.slice(6))))) return null
+      result = ['', str]
+    } else if (str.match(/^(\+|-|\/|\*|<|>|=|<=|>=)/)) {
+      if (!(result = operator(spaceParser(str)))) return null
       str = result[1]
     } else break
   }
   return result
 }
-function evaluate (inp) {
+function evaluater (inp) {
   let str = inp.slice(0); let result; let val
-  if (spaceParser(str.slice(1)) === ')') return ['()', '']
   while (str.length && !str.startsWith(')')) {
     if (str.startsWith('(')) {
-      if (!(result = expParser(spaceParser(str.slice(1))))) return null
+      if (!(result = expression(spaceParser(str.slice(1))))) return null
       str = result[1]
     }
     if ((val = numberParser(str))) {
-      result = [+val[0], val[1]]; break
+      result = val; str = val[1]
+      break
     }
     if ((val = stringParser(str))) {
-      result = (env[val[0]] === undefined ? null : [env[val[0]], val[1]]); break
+      result = (env[val[0]] === undefined ? null : [env[val[0]], val[1]]); str = val[1]; break
     }
     if (!str[0] === ')') str = spaceParser(str.slice(1))
   }
-  return [result[0], spaceParser(str.slice(1))]
+  str = spaceParser(str.slice(1))
+  return [result[0], str]
 }
-function eva (input) {
-  let result = evaluate(input)
-  console.log(result ? result[0] : 'Invalid')
+function eval (input) {
+  let result = evaluater(input)
+  return (result ? result[0] : 'Invalid')
 }
